f90123752cc00d74534906e5bc2e897f46113968
Sebastian Pablo Martin <se.martin@redlink.com.ar>
2022-08-31 10:33:29 -0300

    se suben los cambios recomendados por erik

--- contracts/NFT/MyNft.sol
+++ contracts/NFT/MyNft.sol
@@ -1,18 +1,18 @@
-// contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721 {
+contract MyNFT is ERC721, Ownable {
     uint256 token_count;
     mapping (uint256 => string) imageUri;
     string baseUri = "https://raw.githubusercontent.com/labinnovacion/poc-prode-qatar-2022/main/contracts/NFT/images/"; 
-    address private owner;
 
     constructor() ERC721("Prode NFT", "PNFT") {
-        owner = msg.sender; //colocar el address del contrato del prode
+        
+        //El contrato ahora es Ownable, Ownable tiene su propio contructor, asi que el msg.sender es el owner, cuando quieras pasarlo el contrato de Maty, usas la transaccion transferOwnership y le mandas la dire del contrato de maty.
+        //owner = msg.sender; //colocar el address del contrato del prode
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
@@ -36,14 +36,7 @@ contract MyNFT is ERC721 {
         return _baseURI();
     }
 
-    /*function mintNFT(address to, string memory uri) public
-    {
-        token_count  += 1;
-        imageUri[token_count] = uri;
-        _mint(to, token_count);
-        
-    }*/
-    function mintNFT(address to, string memory idMatch) public isContractMati
+    function mintNFT(address to, string memory idMatch) public onlyOwner
     {
         string memory finalUrl = string(abi.encodePacked(idMatch,".txt"));
         token_count  += 1;
@@ -51,10 +44,4 @@ contract MyNFT is ERC721 {
         _mint(to, token_count);
         
     }
-
-    modifier isContractMati() {
-        require(owner == msg.sender);
-        _;
-    }
-
 }
